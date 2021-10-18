0864cff858e3f0bde41d2eb8bdcfa0ab87185d47
Elliott Olson <elolson@iastate.edu>
2021-10-17 20:53:28 -0500

    Setting up the process header

--- include/proc.h
+++ include/proc.h
@@ -1,4 +1,19 @@
 #ifndef proc_h
 #define proc_h
 
+enum state {
+    NEW, READY, RUNNING, WAITING, TERMINATED
+};
+
+struct process {
+    long pid;
+    enum state state;
+    
+    struct process *parent;
+};
+
+struct cpu {
+    struct process *process;
+};
+
 #endif
